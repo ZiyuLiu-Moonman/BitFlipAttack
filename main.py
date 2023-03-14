d17d7afac3f52eb924e4db5d998c3032e033a251
ZiyuLiu <49537782+MoonmanBye@users.noreply.github.com>
2023-03-14 14:46:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,8 @@ def train(loader, model, criterion, optimizer, epoch, C):
 
         loss.backward(retain_graph=True)
         
-        '''
+        grad_1 = model.module.linear.weight.grad.clone()
+        
         loss_grad_conv = GenerateLoss(model.module.conv1.weight.grad, rand_fix_conv)
         #res block1
         loss_grad_conv101 = GenerateLoss(model.module.layer1[0].conv1.weight.grad, rand_fix_conv101)
@@ -193,7 +194,9 @@ def train(loader, model, criterion, optimizer, epoch, C):
         
         loss_layer = 100 * (loss_grad_conv+ loss_grad_conv101 + loss_grad_conv102 + loss_grad_conv111+ loss_grad_conv112+ loss_grad_conv121+ loss_grad_conv122+ loss_grad_conv201+ loss_grad_conv202+ loss_grad_conv211+loss_grad_conv212+ loss_grad_conv221+ loss_grad_conv222+ loss_grad_conv301+ loss_grad_conv302+loss_grad_conv311+ loss_grad_conv312+ loss_grad_conv321+ loss_grad_conv322+ loss_grad_linear)
         loss_layer.backward()
-        '''
+        
+        grad_2 = model.module.linear.weight.grad.clone()
+        print(grad_1.equal(grad_2))
         
         '''
         #add noise
